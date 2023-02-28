37fa2de93fcaae5bbd62064097e8f1eb94dc70a8
Inc21 <intc21@gmail.com>
2023-02-28 22:07:03 +0000

    Get elements and add event listeners to options.js

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,3 +1,4 @@
+// get elements
 const options = document.getElementsByClassName("options");
 const gameOverArea = document.getElementsByClassName("gameOver");
 const compBtn = document.getElementById("computer");
@@ -6,4 +7,31 @@ const xBtn = document.getElementById("xBtn");
 const oBtn = document.getElementById("oBtn");
 const playBtn = document.getElementById("playBtn");
 
-console.log("holaaa");
+// variables for options
+let OPPONENT;
+const player = new Object;
+
+// add event listeners to the buttons
+compBtn.addEventListener("click", function() {
+    OPPONENT = "computer";
+});
+
+friendBtn.addEventListener("click", function() {
+   OPPONENT = "friend";
+});
+
+xBtn.addEventListener("click", function() {
+    player.man = "X";
+    player.computer = "O";
+    player.friend = "O";
+});
+
+oBtn.addEventListener("click", function() {
+    player.man = "O";
+    player.computer = "X";
+    player.friend = "X";
+});
+
+playBtn.addEventListener("click", function() {
+    
+});
