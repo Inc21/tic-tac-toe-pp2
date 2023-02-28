56eae593eb214e26cee5463b5756f93a5f7bf310
Inc21 <intc21@gmail.com>
2023-02-28 21:46:05 +0000

    Add js empty files and some styling to the options section

---
+++ assets/js/options.js
@@ -0,0 +1,9 @@
+const options = document.getElementsByClassName("options");
+const gameOverArea = document.getElementsByClassName("gameOver");
+const compBtn = document.getElementById("computer");
+const friendBtn = document.getElementById("friend");
+const xBtn = document.getElementById("xBtn");
+const oBtn = document.getElementById("oBtn");
+const playBtn = document.getElementById("playBtn");
+
+console.log("holaaa");
