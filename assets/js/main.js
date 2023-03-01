6d82366da2a24937bd6f27f2d6fd62c6a5e681b9
Inc21 <intc21@gmail.com>
2023-03-01 23:28:54 +0000

    Get main game elements, add eventListener to the boxes on the board

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,33 @@
 function init(player, OPPONENT) {
+    let boxes = document.getElementsByClassName("box");
+    const spaces = [];
+    const O = "O";
+    const X = "X";
+    let currentPlayer = player.man;
 
+    const boxClicked = (e) => {
+        const id = e.target.id;
+        if (!spaces[id]) {
+            spaces[id] = currentPlayer;
+            e.target.innerText = currentPlayer;
+            let container = document.getElementById("gameBoard");
+            if (playerHasWon()) {
+                boxes.forEach((box) => {
+                    box.removeEventListener("click", boxClicked);
+                })
+            } else {
+                currentPlayer = currentPlayer === O ? X : O;
+            }  
+        }
+    };
+
+    const box = boxes;
+        for (i = 0; i < boxes.length; i++) {
+            box[i].addEventListener("click", boxClicked);
+        }
+    
+       
+
+    
+    
 }
