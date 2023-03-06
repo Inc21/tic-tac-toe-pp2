823349529dbb4772f55b15399cd2901cec42d6b8
Inc21 <intc21@gmail.com>
2023-03-06 23:18:38 +0000

    add meta tags to HTML

--- assets/js/rules.js
+++ assets/js/rules.js
@@ -1,8 +1,8 @@
 // Function to hide the rules
 function showHide() {
-    let rule = document.querySelector(".gameRules");
-    let btn = document.getElementById("rulesBtn");
-    let cont = document.getElementById("container");
+    const rule = document.querySelector(".gameRules");
+    const btn = document.getElementById("rulesBtn");
+    const cont = document.getElementById("container");
     
     if(rule.style.display === "block") {
         rule.style.display = "none";
@@ -10,10 +10,7 @@ function showHide() {
         cont.style.minHeight = "calc(100vh - 14.5rem)";
     } else {
         rule.style.display = "block";
-        btn.innerHTML = "Hide Rules"
+        btn.innerHTML = "Hide Rules";
         rule.classList.remove("hide");
-        // cont.style.minHeight = "auto";
-        // rule.style.marginTop = "-120px";
-        // btn.style.margin = "-520px";
     }
 }
